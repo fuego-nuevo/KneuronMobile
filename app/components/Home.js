ff509808213b33bd86abc6d65f754ceeea5186ab
J6K <jmkang06@gmail.com>
2017-05-29 01:47:29 -0700

    [Fix] sockets listens on home mount

--- app/components/Home.js
+++ app/components/Home.js
@@ -8,10 +8,17 @@ import {
 } from 'react-native';
 import { Container, View, Icon, DeckSwiper, Card, CardItem, Thumbnail, Text, Left, Body, Footer, FooterTab, Button } from 'native-base';
 import { Actions } from 'react-native-router-flux';
+import io from 'socket.io-client';
 import NavBar from './NavBar/NavBar';
 import CohortList from './Cohorts/CohortList';
 
-export default class Test extends Component {
+
+const socket = io('http://localhost:5000');
+
+export default class Home extends Component {
+  componentDidMount() {
+    socket.on('live-lecture');
+  }
   render() {
     const { container } = styles;
     return (
@@ -29,6 +36,5 @@ const styles = {
     position: 'relative',
     flex: 1,
     backgroundColor: '#dcdfe5',
-  }
-}
-
+  },
+};

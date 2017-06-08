47aa048b15d79af39ff037bce6c5cdf68cdbcbb4
Jason Kim <jasonkim3068@gmail.com>
2017-06-07 23:33:51 -0700

    took out proximity

--- app/components/Camera/CameraRoute.js
+++ app/components/Camera/CameraRoute.js
@@ -5,14 +5,14 @@ import {
   StyleSheet,
   Text,
   TouchableHighlight,
-  View
+  View,
 } from 'react-native';
 import Camera from 'react-native-camera';
 import RNFS from 'react-native-fs';
 import axios from 'axios';
 import io from 'socket.io-client';
 import { connect } from 'react-redux';
-import { LatLonEllipsoidal } from 'geodesy';
+// import { LatLonEllipsoidal } from 'geodesy';
 import Config from 'react-native-config';
 import Toast from 'react-native-simple-toast';
 
@@ -70,103 +70,68 @@ class CameraRoute extends Component {
   // }
 
   sendToKairo() {
-    if ('geolocation' in navigator) {
-      navigator.geolocation.getCurrentPosition((position) => {
-        console.log("inside  coord loc baby!: ", position.coords.latitude);
-        const { id, lat, lng } = this.props.currentLecture;
-        let withinClassRange = false;
-        const studentLat = position.coords.latitude;
-        const studentLng = position.coords.longitude;
-      
-
-        let p1 = new LatLonEllipsoidal(studentLat, studentLng);
-        console.log("typeof p1 is: ", typeof p1);
-        // p1 = Math.round(p1, -6);
-        // console.log("p1 rounded is: ", p1);
-        p1.lat = Number(p1.lat.toFixed(13));
-        p1.lon = Number(p1.lon.toFixed(13));
-        let p2 = new LatLonEllipsoidal(lat, lng);
-        console.log("p2 is: ", p2);
-        p2.lat = Number(p2.lat.toFixed(13))
-        p2.lon = Number(p2.lon.toFixed(13))
-        console.log("p1 below then p2 below p1");
-        console.log(p1);
-        console.log(p2);
-        const d = p1.distanceTo(p2);
-        console.log("this is d", d)
-        console.log("this is p1", p1);
-        console.log("this is p2", p2);
-        if (d < 200) {
-          withinClassRange = true;
-          console.log('Student Has entered the classroom');
-        } else {
-          console.log('Student is not near the Classroom');
-        }
-
-        this.camera.capture()
-          .then((data) => {
-            console.log('this is the data from taking kairo photopic', data);
-            RNFS.readFile(data.path, 'base64')
-            .then(res => {
-              console.log('this is the res of the readfile', typeof res);
-              const body = {
-                image: res,
-                subject_id: this.props.profile.username,
-                gallery_name: 'kneuron1',
+    this.camera.capture()
+      .then((data) => {
+        console.log('this is the data from taking kairo photopic', data);
+        RNFS.readFile(data.path, 'base64')
+        .then(res => {
+          console.log('this is the res of the readfile', typeof res);
+          const body = {
+            image: res,
+            subject_id: this.props.profile.username,
+            gallery_name: 'kneuron1',
+          };
+          axios.post(`${Config.Local_Host}/api/facialVerify`, body)
+          .then(res => {
+            console.log('this is the verification for kairo sent pic ', res);
+            if (res.data.images[0].transaction.confidence > .60 ) {
+              this.setState({present: true})
+              console.log('you are who you say you are');
+              Toast.show('You have been marked present!', Toast.LONG);
+              let attendance = {
+                lecture_id: this.props.currentLecture.id,
+                student_id: this.props.profile.id,
+                present: true,
               };
-              axios.post(`${Config.Local_Host}/api/facialVerify`, body)
+              axios.post(`${Config.Local_Host}/api/studentAttendance`, attendance)
               .then(res => {
-                console.log('this is the verification for kairo sent pic ', res);
-                if (res.data.images[0].transaction.confidence > .60 && withinClassRange === true) {
-                  this.setState({present: true})
-                  console.log('you are who you say you are');
-                  Toast.show('You have been marked present!', Toast.LONG);
-                  let attendance = {
-                    lecture_id: this.props.currentLecture.id,
-                    student_id: this.props.profile.id,
-                    present: true,
-                  };
-                  axios.post(`${Config.Local_Host}/api/studentAttendance`, attendance)
-                  .then(res => {
-                    console.log('this is the res from posting attendance', res);
-                    socket.emit('student-track', {
-                    name: this.props.profile.fName + " " + this.props.profile.lName,
-                    present: true,
-                    teacher: this.props.teacher.teacher_id,
-                  });
-
-                  });
-                } else {
-                  this.setState({present: false})
-                  let attendance = {
-                    lecture_id: this.props.currentLecture.id,
-                    student_id: this.props.profile.id,
-                    present: false,
-                  };
-                  axios.post(`${Config.Local_Host}/api/studentAttendance`, attendance)
-                  .then(res => {
-                    console.log('this is the res from posting attendance', res);
-                  Toast.show('You have been marked absent!', Toast.LONG); 
-                  socket.emit('student-track', {
-                    name: this.props.profile.fName + " " + this.props.profile.lName,
-                    present: false,
-                    teacher: this.props.teacher.teacher_id,
-                  });              
-                  });
-                  console.log('who the fuckk are you broo');
-                }
-            // .catch(err => {
-            //   console.log("there was an error verifying the kairo pic ", err);
-            // })
-              })
-              .catch((err) => {
-                console.log('this is err for verifying kairo', err);
+                console.log('this is the res from posting attendance', res);
+                socket.emit('student-track', {
+                name: this.props.profile.fName + " " + this.props.profile.lName,
+                present: true,
+                teacher: this.props.teacher.teacher_id,
               });
-            });
+
+              });
+            } else {
+              this.setState({present: false})
+              let attendance = {
+                lecture_id: this.props.currentLecture.id,
+                student_id: this.props.profile.id,
+                present: false,
+              };
+              axios.post(`${Config.Local_Host}/api/studentAttendance`, attendance)
+              .then(res => {
+                console.log('this is the res from posting attendance', res);
+              Toast.show('You have been marked absent!', Toast.LONG); 
+              socket.emit('student-track', {
+                name: this.props.profile.fName + " " + this.props.profile.lName,
+                present: false,
+                teacher: this.props.teacher.teacher_id,
+              });              
+              });
+              console.log('who the fuckk are you broo');
+            }
+        // .catch(err => {
+        //   console.log("there was an error verifying the kairo pic ", err);
+        // })
           })
-          .catch(err => console.error(err));
-    });
-    }
+          .catch((err) => {
+            console.log('this is err for verifying kairo', err);
+          });
+        });
+      })
+      .catch(err => console.error(err));
   }
 
 
